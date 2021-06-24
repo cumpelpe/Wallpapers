2771097282d9ea96d6836561007055affad49aab
Petr Čumplelík <cumpelpe@gmail.com>
2021-06-24 23:13:32 +0200

    Make Tomato happy

--- animated/stop_animated_backgrounds.sh
+++ animated/stop_animated_backgrounds.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+# File              : stop_animated_backgrounds.sh
+# Author            : Petr Čumpelík <cumpepe1@fit.cvut.cz>
+# Date              : 24.06.2021
+# Last Modified Date: 24.06.2021
+# Last Modified By  : Petr Čumpelík <cumpepe1@fit.cvut.cz>
 #!/usr/bin/env bash
 
-for pid in $(pidof xwinwrap | tr " " "\n")
-do
-	kill $pid
-done
+pkill xwinwrap
