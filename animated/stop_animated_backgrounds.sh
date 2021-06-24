0ca1b962e94d0d5366f419790dd2841d63d26134
cumpelpe <35816455+cumpelpe@users.noreply.github.com>
2021-06-24 23:11:06 +0200

    Editor playing pranks on me

--- animated/stop_animated_backgrounds.sh
+++ animated/stop_animated_backgrounds.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # File              : stop_animated_backgrounds.sh
 # Author            : Petr Čumpelík <cumpepe1@fit.cvut.cz>
 # Date              : 24.06.2021
 # Last Modified Date: 24.06.2021
 # Last Modified By  : Petr Čumpelík <cumpepe1@fit.cvut.cz>
-#!/usr/bin/env bash
+
 
 pkill xwinwrap
